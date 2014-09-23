09bf8b60d915540659e279fd63be845f7004471e
Damian Knapik <ddknapik@gmail.com>
2014-09-23 10:13:35 +0200

    developing remote call

--- src/scripts/local_connection.js
+++ src/scripts/local_connection.js
@@ -1,50 +1,100 @@
-(function () {
-    var ui, localStream, showPreview, setupLocalVideo, showTips;
+var ui, localStream, setupLocalVideo, showTips,
+    sendChannel, receiveChannel, sdpConstraints, mediaConstraints,
+    webrtcDetectedBrowser, isInitiator;
 
-    ui = {
-        $sidebar: $('#sidebar'),
-        $main: $('#main'),
-        $previewBtn: $('#preview-btn'),
-        $jumbotron: $('.jumbotron')
-    };
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    ui.$previewBtn.click(function () {
-        ui.$jumbotron.html('<video id="local-video" autoplay></div>')
-        showPreview();
-    });
-
-    setupLocalVideo = function (stream, $videoEl) {
-        var videoSrc;
-        localStream = stream
-        videoSrc    = window.URL ? URL.createObjectURL(stream) : stream;
-        $videoEl.attr('src', videoSrc);
-        $videoEl.show();
-    };
-
-    showTips = function () {
-        ui.$jumbotron.append(
-            '<h1>Great job!</h1>' +
-            '<p>What are you waiting for? Just call any of your friends listed on the left!</p>'
-        );
+peerConnectionConfig = (function () {
+    if (webrtcDetectedBrowser === 'firefox') {
+        return {'iceServers': [{'url':'stun:23.21.150.121'}]};
+    } else {
+        return {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
     }
-
-    showPreview = function () {
-        var $videoEl;
-        console.log('Obtaining access to camera and microphone.');
-        $videoEl = $('#local-video');
-        $videoEl.hide();
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-        
-        navigator.getUserMedia({
-            audio: true,
-            video: true
-        }, function (stream) {
-            console.log('Local stream obtained.');
-            setupLocalVideo(stream, $videoEl);
-            showTips();
-        }, function (err) {
-            console.log(err);
-        });
-    }
-
 }());
+
+peerConnectionConstraints = {
+    'optional': [
+        { 'DtlsSrtpKeyAgreement': true }
+    ]
+};
+
+sdpConstraints = {};
+mediaConstraints = { video: true, audio: true };
+
+ui = {
+    $sidebar: $('#sidebar'),
+    $main: $('#main'),
+    $previewBtn: $('#preview-btn'),
+    $remoteVide: $('#remote-video'),
+    $jumbotron: $('.jumbotron'),
+    $nameForm: $('#name-form'),
+    $activeUsers: $('#active-users tbody'),
+    $currentName: $('#current-name')
+};
+
+window.onbeforeunload = function () {
+    hangup();
+}
+
+ui.$previewBtn.click(function () {
+    ui.$jumbotron.html('<video id="local-video" autoplay></div>')
+    showPreview();
+});
+
+setupLocalVideo = function (stream, $videoEl) {
+    var videoSrc;
+    localStream = stream
+    videoSrc    = window.URL ? URL.createObjectURL(stream) : stream;
+    $videoEl.attr('src', videoSrc);
+    $videoEl.show();
+};
+
+showTips = function () {
+    ui.$jumbotron.append(
+        '<h1>Great job!</h1>' +
+        '<p>What are you waiting for? Just call any of your friends listed on the left!</p>'
+    );
+}
+
+function handleLocalStream (stream) {
+    console.log('Local stream obtained.');
+    setupLocalVideo(stream, $videoEl);
+    showTips();
+}
+
+function handleLocalStreamError (err) {
+    console.log(err);
+}
+
+function showPreview() {
+    var $videoEl;
+    console.log('Obtaining access to camera and microphone.');
+    $videoEl = $('#local-video');
+    $videoEl.hide();
+    navigator.getUserMedia(mediaConstraints, handleLocalStream, handleLocalStreamError);
+    setupPeerConnection();
+}
+
+function setupPeerConnection () {
+    if (isStarted || typeof localStream === 'undefined' || !isChannelReady) {
+        return;
+    }
+    connection = new RTCPeerConnection(peerConnectionConfig, peerConnectionConstraints);
+    connection.addStream(localStream);
+    connection.onicecandidate = handleIceCandidate;
+    console.log('Created RTCPeerConnnection');
+    connection.onaddstream = handleRemoteStreamAdded;
+    connection.onremovestream = handleRemoteStreamRemoved;
+    if (isInitiator) {
+        // Create a reliable data channel
+        sendChannel = connection.createDataChannel("sendDataChannel", {reliable: true});
+        trace('Created send data channel');
+        sendChannel.onopen = handleSendChannelStateChange;
+        sendChannel.onmessage = handleMessage;
+        sendChannel.onclose = handleSendChannelStateChange;
+    } else { // Joiner
+        connection.ondatachannel = gotReceiveChannel;
+    }
+
+    isStarted = true;
+}
--- src/scripts/sockets.js
+++ src/scripts/sockets.js
@@ -1,79 +1,74 @@
-(function () {
-    var socket, ui, currentName, updateCurrentName, bindActiveUsers;
+var socket, ui, currentName, updateCurrentName, bindActiveUsers;
 
-    socket = io();
+socket = io();
 
-    ui = {
-        $nameForm: $('#name-form'),
-        $activeUsers: $('#active-users tbody'),
-        $currentName: $('#current-name')
-    }
+ui.$nameForm.submit(function (evt) {
+    var name;
+    evt.preventDefault();
+    name = $(this).find('#name').val();
+    if (name.length === 0) { return false; }
+    
+    socket.emit('rename', name);
+    return false;
+});
 
-    ui.$nameForm.submit(function (evt) {
-        var name;
-        evt.preventDefault();
-        name = $(this).find('#name').val();
-        if (name.length === 0) { return false; }
-        
-        socket.emit('rename', name);
-        return false;
+bindActiveUsers = function () {
+    $('.call').click(function () {
+        var recipient;
+        recipient = $(this).data('user');
+        socket.emit('call', recipient);
+        ui.$jumbotron.html('<video id="local-video" autoplay></div>')
+        showPreview();
     });
+}
 
-    bindActiveUsers = function () {
-        $('.call').click(function () {
-            var recipient;
-            recipient = $(this).data('user');
-            socket.emit('call', recipient)
-        });
-    }
+updateCurrentName = function (name) {
+    currentName = name;
+    ui.$currentName.text(name);
+    ui.$nameForm.find('#name').val('');
+}
 
-    updateCurrentName = function (name) {
-        currentName = name;
-        ui.$currentName.text(name);
-        ui.$nameForm.find('#name').val('');
-    }
+socket.on('callReceived', function (res) {
+    console.log(res.callee, ' wants to talk with you.');
+    console.log('The conversation identifier is', res.room);
+    socket.emit('answerCall', res.room);
+});
 
-    socket.on('callReceived', function (res) {
-        console.log(res.callee + ' wants to talk with you.');
-        socket.emit('answerCall', res.room);
-    });
+socket.on('idAssigned', function (res) {
+    updateCurrentName(res.name);
+    console.log('Hey guest! You are now known as ' + res.name);
+});
 
-    socket.on('idAssigned', function (res) {
+socket.on('peerJoins', function (res) {
+    console.log('socket ' + res.socketId + ' joined this channel.');
+});
+
+socket.on('created', function (res) {
+    console.log('socket ' + res.socketId + ' joined channel ' + res.channel);
+});
+
+socket.on('message', function (res) {
+    console.log('-- MESSAGE -- ' + res.body);
+});
+
+socket.on('renameResult', function (res) {
+    if (res.success) {
         updateCurrentName(res.name);
-        console.log('Hey guest! You are now known as ' + res.name);
-    });
+        console.log('Your name has been changed to ' + res.name);
+    } else {
+        console.log(res.message);
+    }
+});
 
-    socket.on('peerJoins', function (res) {
-        console.log('socket ' + res.socketId + ' joined this channel.');
+socket.on('activeUsers', function (res) {
+    var list;
+    list = _.map(res.users, function (userName) {
+        if (userName === currentName) { return ''; }
+        return '<tr>' +
+                    '<td>' + userName + '</td>' +
+                    '<td><span data-user="' + userName + '" class="call glyphicon glyphicon-earphone"></td>' +
+                '</tr>';
     });
-
-    socket.on('created', function (res) {
-        console.log('socket ' + res.socketId + ' joined channel ' + res.channel);
-    });
-
-    socket.on('message', function (res) {
-        console.log('-- MESSAGE -- ' + res.body);
-    });
-
-    socket.on('renameResult', function (res) {
-        if (res.success) {
-            updateCurrentName(res.name);
-            console.log('Your name has been changed to ' + res.name);
-        } else {
-            console.log(res.message);
-        }
-    });
-
-    socket.on('activeUsers', function (res) {
-        var list;
-        list = _.map(res.users, function (userName) {
-            if (userName === currentName) { return ''; }
-            return '<tr>' +
-                        '<td>' + userName + '</td>' +
-                        '<td><span data-user="' + userName + '" class="call glyphicon glyphicon-earphone"></td>' +
-                    '</tr>';
-        });
-        ui.$activeUsers.html(list);
-        bindActiveUsers();
-    });
-}());
+    ui.$activeUsers.html(list);
+    bindActiveUsers();
+});
