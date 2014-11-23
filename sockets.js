13edf1f5be59e2bccc067787994dec7270b3f89b
Damian Knapik <ddknapik@gmail.com>
2014-11-23 20:07:30 +0100

    swap js files

--- sockets.js
+++ sockets.js
@@ -8,128 +8,38 @@ var socketio      = require('socket.io'),
     io, assignGuestId, otherUsersInRoom, joinRoom,
     usernamesInRoom, refreshActiveUsers;
 
-function assignGuestId(socket) {
-    var name;
-    name = 'Guest#' + guestId;
-    usernames[socket.id] = name;
-    namesTaken.push(name);
-    socket.emit('idAssigned', {
-        name: name
-    });
-    ++guestId;
-}
-
-function usernamesInRoom(room) {
-    var users, collectedUsernames = [];
-    users = Object.keys( io.nsps['/'].adapter.rooms[room] || {} );
-    for (var index in users) {
-        var socketId = users[index];
-        collectedUsernames.push(usernames[socketId]);
-    }
-    return collectedUsernames;
-}
-
-function otherUsersInRoom(room, socket) {
-    var users, message;
-    users = Object.keys( io.nsps['/'].adapter.rooms[room] || {} );
-    if (users.length === 1) { return 'You are the very first user here.'; }
-    message = 'Other users in the room: ';
-    for (var index in users) {
-        var socketId = users[index];
-        if (socketId === socket.id) { continue; }
-        if (index > 0) { message += ', '; }
-        message  += usernames[socketId];
-    }
-    return message;
-}
-
-function joinRoom(socket, room) {
-    socket.join(room);
-    currentRoom[socket.id] = room;
-    socket.emit('joinRoom', { room: room });
-    socket.broadcast.to(room).emit('message', {
-        body: usernames[socket.id] + ' has joined ' + room + '.'
-    });
-    socket.emit('message', {
-        body: otherUsersInRoom(room, socket)
-    });
-    refreshActiveUsers(room);
-}
-
-function refreshActiveUsers() {
-    io.emit('activeUsers', {
-        users: usernamesInRoom('Public')
-    });
-}
-
 exports.listen = function (server) {
-    io = socketio.listen(server);
-    
-    io.on('connection', function (socket) {
-        assignGuestId(socket);
-        joinRoom(socket, 'Public');
-        
-        socket.on('disconnect', function () {
-            var idIndex;
-            idIndex = namesTaken.indexOf(usernames[socket.id]);
-            delete namesTaken[idIndex];
-            delete usernames[socket.id];
+    io.sockets.on('connection', function (socket) {
+        socket.on('message', function (message) {
+            log('# got message: ', message);
+            // channel-only broadcast
+            socket.broadcast.to(message.channel).emit('message', message);
         });
 
-        socket.on('answerCall', function (conversationId) {
-            var conversation;
-            conversation = _.find(conversations, function (el) {
-                return el.room === conversationId;
-            });
-            io.sockets.connected[conversation.caller].emit('message', {
-                body: 'Jest nieźle :)'
-            });
-            socket.emit('joined', conversationId);
-        });
+        socket.on('create or join', function (room) {
+            var numClients = io.sockets.clients(room).length;
 
-        socket.on('call', function (recipient) {
-            var recipientId, randomNumber, conversationId, recipientSocket;
-            
-            randomNumber   = Math.floor((Math.random() * 9999) + 1);
-            conversationId = randomNumber.toString();
-            recipientId    = _.findKey(usernames, function (name) {
-                return name === recipient;
-            });
-            
-            conversations.push({
-                caller: socket.id,
-                recipient: recipientId,
-                room: conversationId
-            });
-            recipientSocket = io.sockets.connected[recipientId];
-            recipientSocket.emit('callReceived', {
-                caller: usernames[socket.id],
-                room: conversationId
-            });
-        });
-        
-        socket.on('rename', function (name) {
-            var previousName, previousNameIdx;
-            if (namesTaken.indexOf(name) !== -1) {
-                socket.emit('renameResult', {
-                    success: false,
-                    message: name + 'has already been taken'
-                });
+            log('# Room' + room + ' has ' + numClients + ' client(s)');
+            log('# Request to create or join room', room);
+
+            if (numClients === 0) {
+                socket.join(room);
+                socket.emit('created', room);
+            } else if (numClients === 1) {
+                io.sockets.in(room).emit('join', room);
+                socket.join(room);
+                socket.emit('joined', room);
+            } else {
+                socket.emit('full', room);
             }
-            previousName = usernames[socket.id];
-            previousNameIdx = namesTaken.indexOf(previousName);
-            namesTaken.push(name);
-            usernames[socket.id] = name;
-            delete namesTaken[previousNameIdx];
-            socket.emit('renameResult', {
-                success: true,
-                name: name
-            });
-            refreshActiveUsers(currentRoom[socket.id]);
-            socket.broadcast.to(currentRoom[socket.id]).emit('message', {
-                body: previousName + ' changed his name to ' + name
-            });
         });
-    });
 
+        function log() {
+            var array = ['>>>> '];
+            for (var i = arguments.length - 1; i >= 0; i =- 1) {
+                array.push(arguments[i]);
+            }
+            socket.emit('log', array);
+        }
+    });
 };
--- src/scripts/local_connection.js
+++ src/scripts/local_connection.js
@@ -1,99 +1,303 @@
-/* global $ */
-var ui, localStream, setupLocalVideo,
-    sendChannel, sdpConstraints, mediaConstraints,
-    webrtcDetectedBrowser, isInitiator, isStarted,
-    isChannelReady, peerConnectionConfig, peerConnectionConstraints;
+/* global webrtcDetectedBrowser:false, io:false */
+'use strict';
 
-isStarted = false;
-isChannelReady = false;
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-peerConnectionConfig = (function () {
-    if (webrtcDetectedBrowser === 'firefox') {
-        return {'iceServers': [{'url':'stun:23.21.150.121'}]};
-    } else {
-        return {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
-    }
-}());
-
-peerConnectionConstraints = {
-    'optional': [
-        { 'DtlsSrtpKeyAgreement': true }
-    ]
-};
-
-sdpConstraints = {};
-mediaConstraints = { video: true, audio: true };
-
-ui = {
-    $sidebar: $('#sidebar'),
-    $main: $('#main'),
-    $previewBtn: $('#preview-btn'),
-    $remoteVide: $('#remote-video'),
-    $jumbotron: $('.jumbotron'),
-    $nameForm: $('#name-form'),
-    $activeUsers: $('#active-users tbody'),
-    $currentName: $('#current-name'),
-    $localVideo: $('#local-video'),
-    $remoteVideo: $('#remote-video')
-};
-
-function hangup() {
-    console.log('end');
-}
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
 
 window.onbeforeunload = function () {
     hangup();
 };
 
-setupLocalVideo = function (stream) {
-    var videoSrc;
-    localStream = stream;
-    videoSrc    = window.URL ? URL.createObjectURL(stream) : stream;
-    ui.$localVideo.attr('src', videoSrc);
-    ui.$localVideo.show();
+var sendChannel, receiveChannel, sendButton, sendTextarea, receiveTextarea,
+    localVideo, remoteVideo, isChannelReady, isInitiator, isStarted,
+    localStream, remoteStream,
+    // Peer Connection
+    pc, pc_config, pc_constraints, sdpConstraints,
+    room, socket,
+    // getUserMedia constraints
+    constraints;
+
+sendButton      = document.getElementById('sendButton');
+sendTextarea    = document.getElementById('dataChannelSend');
+receiveTextarea = document.getElementById('dataChannelReceive');
+
+localVideo      = document.querySelector('#localVideo');
+remoteVideo     = document.querySelector('#remoteVideo');
+
+sendButton.onclick = sendData;
+
+isChannelReady = false;
+isInitiator    = false;
+isStarted      = false;
+
+pc_config = (function (browser) {
+    var url;
+    url = browser === 'firefox' ? 'stun:23.21.150.121' : 'stun:stun.l.google.com:19302';
+    return {'iceServers': [{'url': url}]};
+}(webrtcDetectedBrowser));
+
+pc_constraints = {
+    'optional': [{'DtlsSrtpKeyAgreement': true}]
 };
 
-function handleLocalStream (stream) {
-    console.log('Local stream obtained.');
-    setupLocalVideo(stream);
+sdpConstraints = {};
+
+room = prompt('Enter room name:');
+socket = io.connect('http://localhost:3000');
+
+if (room !== '') {
+    console.log('Create or join room', room);
+    socket.emit('create or join', room);
 }
 
-function handleLocalStreamError (err) {
-    console.log(err);
+constraints = {video: true, audio: true};
+
+function handleUserMedia(stream) {
+    localStream = stream;
+    attachMediaStream(localVideo, stream);
+    console.log('Adding local stream');
+    sendMessage('got user media');
 }
 
-function setupPeerConnection () {
-    var connection;
-    console.log('channel is ready?', isChannelReady);
-    if (isStarted || typeof localStream === 'undefined' || !isChannelReady) {
+function handleUserMediaError(error) {
+    console.log('navigator.getUserMedia error:', error);
+}
+
+// Server-mediated message exchanging
+// 1. Server -> CLient
+
+// Handle 'created' message coming back from server:
+// This peer is the initiator
+socket.on('created', function (room) {
+    console.log('Created room', room);
+    isInitiator = true;
+
+    navigator.getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+    console.log('Getting user media with constraints', constraints);
+    checkAndStart();
+});
+
+socket.on('full', function (room) {
+    console.log('Room', room, 'is full.');
+});
+
+socket.on('join', function (room) {
+    console.log('Another peer mage a request to join room', room);
+    console.log('This peer is the initiator of room', room, '!');
+    isChannelReady = true;
+});
+
+socket.on('joined', function (room) {
+    console.log('This peer has joined room', room);
+    isChannelReady = true;
+
+    navigator.getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+    console.log('Getting user media with constraints', constraints);
+});
+
+socket.on('log', function (array) {
+    console.log.apply(console, array);
+});
+
+socket.on('message', function (message) {
+    console.log('Received message:', message);
+    if (message === 'got user media') {
+        checkAndStart();
+    } else if (message.type === 'offer') {
+        if (!isInitiator && !isStarted) {
+            checkAndStart();
+        }
+        pc.setRemoteDescription(new RTCSessionDescription(message));
+        doAnswer();
+    } else if (message.type === 'answer' && isStarted) {
+        pc.setRemoteDescription(new RTCSessionDescription(message));
+    } else if (message.type === 'candidate' && isStarted) {
+        var candidate = new RTCIceCandidate({
+            sdpMLineIndex: message.label,
+            candidate: message.candidate
+        });
+        pc.addIceCandidate(candidate);
+    } else if (message === 'bye' && isStarted) {
+        handleRemoteHangup();
+    }
+});
+
+// 2. Client -> Server
+
+function sendMessage(message) {
+    console.log('Sending message:', message);
+    socket.emit('message', message);
+}
+
+// Channel negotiation trigger function
+function checkAndStart() {
+    if (!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+        createPeerConnection();
+        isStarted = true;
+        if (isInitiator) { doCall(); }
+    }
+}
+
+// Peer Connection management
+function createPeerConnection () {
+    try {
+        pc = new RTCPeerConnection(pc_config, pc_constraints);
+        pc.addStream(localStream);
+        pc.onicecandidate = handleIceCandidate;
+
+        console.log(
+            "Created RTCPeerConnection with:\n" +
+            "config: '" + JSON.stringify(pc_config) + "'\n" +
+            "constraints: '" + JSON.stringify(pc_constraints) + "'"
+        );
+    } catch (e) {
+        console.log('Failed to create PeerConnection, exception: ', e.message);
+        alert('Cannot create RTCPeerConnection object.');
         return;
     }
-    connection = new RTCPeerConnection(peerConnectionConfig, peerConnectionConstraints);
-    connection.addStream(localStream);
-    connection.onicecandidate = handleIceCandidate;
-    console.log('Created RTCPeerConnnection');
-    connection.onaddstream = handleRemoteStreamAdded;
-    connection.onremovestream = handleRemoteStreamRemoved;
+
+    pc.onaddstream = handleRemoteStreamAdded;
+    pc.onremovestream = handleRemoteStreamRemoved;
+
     if (isInitiator) {
-        // Create a reliable data channel
-        sendChannel = connection.createDataChannel("sendDataChannel", {reliable: true});
-        trace('Created send data channel');
-        sendChannel.onopen = handleSendChannelStateChange;
+        try {
+            // Create a reliable data channel
+            sendChannel = pc.createDataChannel('sendDataChannel', {reliable: true});
+            trace('Created send data channel');
+        } catch (e) {
+            alert('Failed to create data channel.');
+            trace('createDataChannel() failed with exception:', e.message);
+        }
+        sendChannel.onopen    = handleSendChannelStateChange;
+        sendChannel.onclose   = handleSendChannelStateChange;
         sendChannel.onmessage = handleMessage;
-        sendChannel.onclose = handleSendChannelStateChange;
-    } else { // Joiner
-        connection.ondatachannel = gotReceiveChannel;
+    } else {
+        // Joiner
+        pc.ondatachannel = gotReceiveChannel;
     }
-    isStarted = true;
 }
 
-function showPreview() {
-    console.log('Obtaining access to camera and microphone.');
-    // ui.$localVideo.hide();
-    isInitiator = true;
-    // isChannelReady = true;
-    navigator.getUserMedia(mediaConstraints, handleLocalStream, handleLocalStreamError);
-    setupPeerConnection();
+// Data channel management
+function sendData() {
+    var data = sendTextarea.value;
+    if (isInitiator) { sendChannel.send(data); }
+    else { receiveChannel.send(data); }
+
+    trace('Sent data: ', data);
+}
+
+// Handlers
+function gotReceiveChannel(event) {
+    trace('Receive Channel Callback');
+    
+    receiveChannel           = event.channel;
+    receiveChannel.onmessage = handleMessage;
+    receiveChannel.onopen    = handleReceiveChannelStateChange;
+    receiveChannel.onclose   = handleReceiveChannelStateChange;
+}
+
+function handleMessage(event) {
+    trace('Received message:', event.data);
+    receiveTextarea.value += event.data +'\n';
+}
+
+function handleSendChannelStateChange() {
+    var readyState = sendChannel.readyState;
+    trace('Send channel state is:', readyState);
+
+    if (readyState === 'open') {
+        dataChannelSend.disabled = false;
+        dataChannelSend.focus();
+        dataChannelSend.placeholder = '';
+        sendButton.disabled = false;
+    } else {
+        dataChannelSend.disabled = true;
+        sendButton.disabled = true;
+    }
+}
+
+function handleReceiveChannelStateChange() {
+    var readyState = receiveChannel.readyState;
+    trace('Receive channel state is:', readyState);
+
+    if (readyState === 'open') {
+        dataChannelSend.disabled = false;
+        dataChannelSend.focus();
+        dataChannelSend.placeholder = '';
+        sendButton.disabled = false;
+    } else {
+        dataChannelSend.disabled = true;
+        sendButton.disabled = true;
+    }
+}
+
+// ICE candidates management
+function handleIceCandidate(event) {
+    console.log('handleIceCandidate event: ', event);
+    if (event.candidate) {
+        sendMessage({
+            type: 'candidate',
+            label: event.candidate.sdpMLineIndex,
+            id: event.candidate.sdpMid,
+            candidate: event.candidate.candidate
+        });
+    } else {
+        console.log('End of candidates.');
+    }
+}
+
+// Create offer
+function doCall() {
+    console.log('Creating Offer...');
+    pc.createOffer(setLocalAndSendMessage, onSignalingError, sdpConstraints);
+}
+
+function onSignalingError(error) {
+    console.log('Failed to create signaling message: ', error.name);
+}
+
+function doAnswer() {
+    console.log('Sending answer to peer.');
+    pc.createAnswer(setLocalAndSendMessage, onSignalingError, sdpConstraints);
+}
+
+// Success handler for both createOffer() and createAnswer()
+function setLocalAndSendMessage(sessionDescription) {
+    pc.setLocalDescription(sessionDescription);
+    sendMessage(sessionDescription);
+}
+
+// Remote stream handlers
+function handleRemoteStreamAdded(event) {
+    console.log('Remote stream added');
+    attachMediaStream(remoteVideo, event.stream);
+    console.log('Remote stream attached.');
+    remoteStream = event.stream;
+}
+
+function handleRemoteStreamRemoved(event) {
+    console.log('Remote stream removed. Event:', event);
+}
+
+// Clean-up functions
+function hangup() {
+    console.log('Hanging up.');
+    stop();
+    sendMessage('Bye!');
+}
+
+function handleRemoteHangup() {
+    console.log('Session terminated');
+    stop();
+    isInitiator = false;
+}
+
+function stop(){
+    isStarted = false;
+    if (sendChannel) { sendChannel.close(); }
+    if (receiveChannel) { receiveChannel.close(); }
+    if (pc) { pc.close(); }
+    pc = null;
+    sendButton.disabled = true;
 }
